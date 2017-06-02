cdee8599b0e6279a2835969763224b01a2ae6416
eggwise <dev@eggwise.com>
2017-06-02 20:50:28 +0200

    setup for resume editing

--- main.py
+++ main.py
@@ -1,18 +1,36 @@
 import json, yaml
 
-FILENAME = 'resume.json'
+
+from lib import generate_resume
+
+FILENAME_JSON = 'resume.json'
+FILENAME_YAML = 'resume.yaml'
+
+
+def json_to_yaml(filename_in, filename_out):
+    with open(filename_in) as f:
+        resume = json.load(f)
+
+    yaml_resume = yaml.dump(resume, default_flow_style=False)
+
+    with open(filename_out, 'w') as f:
+        f.write(yaml_resume)
+
+
+def yaml_to_json(filename_in, filename_out):
+    with open(filename_in) as f:
+        resume = yaml.load(f)
+
+    json_resume = json.dumps(resume)
+
+    with open(filename_out, 'w') as f:
+        f.write(json_resume)
 
 
 
-with open(FILENAME) as f:
+# json_to_yaml(FILENAME_JSON, FILENAME_YAML)
 
-    resume = json.load(f)
+yaml_to_json(FILENAME_YAML, FILENAME_JSON)
 
 
-
-OUTPUT_FILENAME = 'resume.yaml'
-yaml_resume = yaml.dump(resume, default_flow_style=False)
-
-with open(OUTPUT_FILENAME, 'w') as f:
-
-    f.write(yaml_resume)
+generate_resume()
