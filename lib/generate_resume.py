0b6ccceb710336726b5916f2530b47897b957776
eggwise <dev@eggwise.com>
2017-06-05 21:40:58 +0200

    change generate_resume script

--- lib/generate_resume.py
+++ lib/generate_resume.py
@@ -5,10 +5,10 @@ import os
 
 
 def generate_resume():
-    FILENAME = 'resume.html'
+    FILENAME = 'index.html'
     THEME = 'elegant'
 
-    GENERATE_COMMAND = 'hackmyresume BUILD resume.json TO out/resume.html -t jsonresume-theme-{theme} -o options.json'.format(
+    GENERATE_COMMAND = 'hackmyresume BUILD resume.json TO out/{filename} -t jsonresume-theme-{theme} -o options.json'.format(
         filename=FILENAME, theme=THEME)
 
     os.system(GENERATE_COMMAND)
